6bbd6b6aa42bae812c49600a095aa97e494d2941
Vaibhav Goyal <122206348+vaibhavh200@users.noreply.github.com>
2025-04-15 01:08:15 +0530

    Delete frontend directory

--- frontend/src/reportWebVitals.js
+++
@@ -1,9 +0,0 @@
-const reportWebVitals = (onPerfEntry) => {
-  // This is a simplified version that doesn't rely on the web-vitals library
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    // We'll skip actual measurement for now
-    console.log('Performance reporting is disabled');
-  }
-};
-
-export default reportWebVitals; 
